53fdf1536254da4a826b0e718c65064556b96dee
zhangxingwang <zhangxingwang@sinosoft.com.cn>
2016-12-29 11:33:58 +0800

    使用fs模块导入json处理json内容，生成html文件

--- json.js
+++ json.js
@@ -1,4 +1,5 @@
 var fs=require('fs');
+var _=require('lodash');
 fs.readFile('zhihu/1.json',function(err,data){  
     if(err)  
         throw err;  
@@ -9,9 +10,21 @@ fs.readFile('zhihu/1.json',function(err,data){
     var chunks=[];  
     var length=0;  
       
-    
+    var data=jsonObj.data;
+    console.log(data.length);
+    _.times(data.length,function(i){
+    	var answer=new Buffer(data[i].content);
+
+    	fs.appendFile('zhihu/result.html',answer,'utf8',function(err){  
+        if(err) throw err;  
+        console.log('write JSON into TEXT');  
+    });
+
+    })
+    /*
     fs.writeFile('json/result.txt',resultBuffer,function(err){  
         if(err) throw err;  
         console.log('write JSON into TEXT');  
-    });  
+    });
+    */  
 });  
