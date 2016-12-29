48ed7dcaf474bbf2626ff94f26da2abd46a1b596
zhangxingwang <zhangxingwang@sinosoft.com.cn>
2016-12-29 13:49:05 +0800

    使用tomarkdown模块测试将html转为md文件

--- json.js
+++ json.js
@@ -1,5 +1,7 @@
 var fs=require('fs');
 var _=require('lodash');
+var toMarkdown=require('to-markdown');
+/*
 fs.readFile('zhihu/1.json',function(err,data){  
     if(err)  
         throw err;  
@@ -12,19 +14,27 @@ fs.readFile('zhihu/1.json',function(err,data){
       
     var data=jsonObj.data;
     console.log(data.length);
+    
     _.times(data.length,function(i){
     	var answer=new Buffer(data[i].content);
 
-    	fs.appendFile('zhihu/result.html',answer,'utf8',function(err){  
+    	fs.appendFile('zhihu/'+i+'.html',answer,'utf8',function(err){  
         if(err) throw err;  
-        console.log('write JSON into TEXT');  
+        console.log('write JSON into ',i,'.html');  
     });
 
     })
-    /*
-    fs.writeFile('json/result.txt',resultBuffer,function(err){  
-        if(err) throw err;  
-        console.log('write JSON into TEXT');  
-    });
-    */  
-});  
+
+});
+
+*/
+fs.readFile('zhihu/1.html',function(err,data){
+	if(err) throw err;
+	//var jsonObj=JSON.parse(data); 
+	console.log(data);
+	 var data = data.toString('utf8');
+	console.log('string', data);
+	var answer=toMarkdown(data);
+	console.log('markdown',answer);
+}) ; 
+
