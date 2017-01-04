5c6f50f9f2f01e1eb14613c414a7a950b3fd3eb7
zhangxingwang <zhangxingwang@sinosoft.com.cn>
2017-01-04 17:19:47 +0800

    实现所有md文件的输出

--- json.js
+++ json.js
@@ -1,8 +1,9 @@
 var fs=require('fs');
 var _=require('lodash');
 var toMarkdown=require('to-markdown');
-/*
-fs.readFile('zhihu/1.json',function(err,data){  
+for(var j=0;j<1;j++)
+{
+fs.readFile('zhihu/'+j+'.json',function(err,data){  
     if(err)  
         throw err;  
           
@@ -16,18 +17,72 @@ fs.readFile('zhihu/1.json',function(err,data){
     console.log(data.length);
     
     _.times(data.length,function(i){
-    	var answer=new Buffer(data[i].content);
+    	
+    	var answer=toMarkdown(data[i].content);
+        var title=data[i].question.title;
+            var pattern = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）&mdash;—|{}【】‘；：”“'。，、？]") 
+            var rs = ""; 
+            for (var k = 0; k < title.length; k++) { 
+            rs = rs+title.substr(k, 1).replace(pattern, ''); 
+            } 
 
-    	fs.appendFile('zhihu/'+i+'.html',answer,'utf8',function(err){  
+            
+            title=new Buffer(rs);
+
+    	    answer=new Buffer(answer);
+            var excerpt=data[i].excerpt;
+           
+            time=data[i].created_time+'000';
+            time=parseInt(time);
+            var created_time=new Date(time);
+            var year=created_time.getFullYear();
+            var month=created_time.getMonth()+1;
+            month=(month<10?'0'+month:month);
+
+            var day=created_time.getDate(); 
+            day=(day<10?'0'+day:day);
+            var hours=created_time.getHours();
+            hours=(hours<10?'0'+hours:hours);
+            var minutes=created_time.getMinutes();
+            minutes=(minutes<10?'0'+minutes:minutes);
+            var seconds='00';
+            created_time=year+'-'+month+'-'+day +'   '+hours+':'+minutes+':'+seconds;
+            var header='title:'+title+'\n'+'date: '+ created_time+' \n' +'categories: 知乎 \n description: '+excerpt +'\n  --- \n ' 
+            header=new Buffer(header);
+            fs.writeFile('zhihumd/'+title+'.md',header,'utf8',function(err){  
         if(err) throw err;  
-        console.log('write JSON into ',i,'.html');  
+        console.log('write JSON into ',i,'.md');  
+        });
+            /*
+        title: 我一个自动化本科生怎么就做了前端呢？(5)
+date: 2016-12-28  16:55:49 
+categories: 回忆记忆
+*/
+
+    /*
+    function stripscript(s) 
+{ 
+var pattern = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）&mdash;—|{}【】‘；：”“'。，、？]") 
+var rs = ""; 
+for (var i = 0; i < s.length; i++) { 
+rs = rs+s.substr(i, 1).replace(pattern, ''); 
+} 
+return rs; 
+}
+
+
+    */
+
+    	fs.appendFile('zhihumd/'+title+'.md',answer,'utf8',function(err){  
+        if(err) throw err;  
+        console.log('write JSON into ',i,'.md');  
     });
 
     })
 
 });
-
-*/
+}
+/*
 fs.readFile('zhihu/1.html',function(err,data){
 	if(err) throw err;
 	//var jsonObj=JSON.parse(data); 
@@ -43,3 +98,4 @@ fs.readFile('zhihu/1.html',function(err,data){
 	})
 }) ; 
 
+*/
