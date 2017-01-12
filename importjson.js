aeb3f22a4082eea51df8b7cdae56d030845732e4
zhangxingwang <zhangxingwang@sinosoft.com.cn>
2017-01-12 14:34:09 +0800

    use eslint to review code

--- importjson.js
+++ importjson.js
@@ -18,12 +18,12 @@ const headers = {
     DNT: 1,
     Host: 'www.zhihu.com',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-    'Cookie': config.Cookie
+    Cookie: config.Cookie,
 };
 console.log('---------start----------------');
 getJson(url);
 
-/*得到json数据，将json数据写入本地文件*/
+/* 得到json数据，将json数据写入本地文件*/
 function getJson(url) {
     request({
         method: 'GET',
@@ -33,12 +33,12 @@ function getJson(url) {
         timeout: 3000,
     }, (error, response, html) => {
         if (!error) {
-            let answer = JSON.stringify(response.body);
+            const answer = JSON.stringify(response.body);
             let answerObj = JSON.parse(answer);
             answerObj = JSON.parse(answerObj);
-            let totals = answerObj.paging.totals;
-            let jsonNum = parseInt(totals / 20) + 1;
-            let urls = [];
+            const totals = answerObj.paging.totals;
+            const jsonNum = parseInt(totals / 20) + 1;
+            const urls = [];
             for (let i = 0; i < jsonNum; i++) {
                 const url = `https://www.zhihu.com/api/v4/members/${zhihuId}/answers?include=data%5B*%5D.is_normal%2Csuggest_edit%2Ccomment_count%2Ccollapsed_counts%2Creviewing_comments_count%2Ccan_comment%2Ccontent%2Cvoteup_count%2Creshipment_settings%2Ccomment_permission%2Cmark_infos%2Ccreated_time%2Cupdated_time%2Crelationship.voting%2Cis_author%2Cis_thanked%2Cis_nothelp%2Cupvoted_followees%3Bdata%5B*%5D.author.badge%5B%3F(type%3Dbest_answerer)%5D.topics&offset=${20 * i}&limit=20&sort_by=created`;
                 urls.push(url);
@@ -48,7 +48,7 @@ function getJson(url) {
     });
 }
 
-/*循环写入json数据*/
+/* 循环写入json数据*/
 function loopJson(urls) {
     async.forEachOf(urls, (url, key, callback) => {
         request({
--- json.js
+++ json.js
@@ -1,97 +1,75 @@
 const fs = require('fs');
 const _ = require('lodash');
 const toMarkdown = require('to-markdown');
-const config=require('./config.js');
 
+const config = require('./config.js');
 const dir = `./${config.zhihuId}md`;
-for (let j = 0; j < 50; j++) {
-  if (!fs.existsSync(`${config.zhihuId}/${j}.json`)) {
-    break;
-  }
-
-  fs.readFile(`${config.zhihuId}/${j}.json`, (err, res) => {
-    if (err) {
-      throw err;
+for (let j = 0; j < 1000000; j++) {
+    if (!fs.existsSync(`${config.zhihuId}/${j}.json`)) {
+        break;
     }
 
-    const jsonObj = JSON.parse(res);
-    const space = ' ';
-    const newLine = ' . ';
-    const chunks = [];
-    const length = 0;
+    fs.readFile(`${config.zhihuId}/${j}.json`, (err, res) => {
+        if (err) {
+            throw err;
+        }
+        const jsonObj = JSON.parse(res);
+        const data = jsonObj.data;
+        _.times(data.length, (i) => {
+            let answer = toMarkdown(data[i].content);
+            const reg = /<noscript>.*?<\/noscript>/g;
+            const reg2 = /src="(.*?)"/;
+            let src = answer.match(reg);
+            const imageList = [];
+            src = _.compact(src); // 使用lodash ，即便是src为null也能够转为空的数组
+            _.times(src.length, (imageNum) => { imageList.push(`![](${src[imageNum].match(reg2)[1]})`); });
+            _.times(src.length, (imageNum) => { answer = answer.replace(src[j], imageList[j]); });
+            let title = data[i].question.title;
+            const pattern = new RegExp("[`~!@#$^&'*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）&mdash;—|{}【】‘；：”“'。，、？]");
+            let rs = '';
+            _.times(title.length, (k) => {
+                const rs2 = title.substr(k, 1).replace(/\"/, ''); // 使用正则表达式单独去除双引号
+                rs += rs2.replace(pattern, '');
+            });
+            title = new Buffer(rs);
 
-    const data = jsonObj.data;
-     _.times(data.length, (i) => {
-      let answer = toMarkdown(data[i].content);
-      let reg=/<noscript>.*?<\/noscript>/g;
-      let reg2=/src="(.*?)"/;
-      let src=answer.match(reg);
-      
-      let imageList=[];
-      src=_.compact(src);  //使用lodash ，即便是src为null也能够转为空的数组
-      for(let imageNum=0;imageNum<src.length;imageNum++)
-      {
-        
-        imageList.push(`![](${src[imageNum].match(reg2)[1]})`);  //["src='https://pic4.zhimg.com/5cb36eebd4f43757024147226aa6c4bf_b.jpg'", "https://pic4.zhimg.com/5cb36eebd4f43757024147226aa6c4bf_b.jpg"]
-      }
-     
-      for(let j=0;j<src.length;j++)
-      {
-        answer=answer.replace(src[j],imageList[j]);
+            answer = new Buffer(answer);
+            const excerpt = data[i].excerpt;
 
-      }
-      
- 
-      let title = data[i].question.title;
-      const pattern = new RegExp("[`~!@#$^&'*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）&mdash;—|{}【】‘；：”“'。，、？]");
-      let rs = '';
-      for (let k = 0; k < title.length; k++) {
-       var rs2=title.substr(k,1).replace(/\"/,'');  //使用正则表达式单独去除双引号
-        rs += rs2.replace(pattern, '');
+            let time = `${data[i].created_time}000`;
 
-      }
-
-
-      title = new Buffer(rs);
-
-      answer = new Buffer(answer);
-      const excerpt = data[i].excerpt;
-
-      let time = `${data[i].created_time}000`;
-
-      time = parseInt(time);
-      const createdTime = new Date(time);
-      const year = createdTime.getFullYear();
-      let month = createdTime.getMonth() + 1;
-      month = (month < 10 ? `0${month}` : month);
-      let day = createdTime.getDate();
-      day = (day < 10 ? `0${day}` : day);
-      let hours = createdTime.getHours();
-      hours = (hours < 10 ? `0${hours}` : hours);
-      let minutes = createdTime.getMinutes();
-      minutes = (minutes < 10 ? `0${minutes}` : minutes);
-      const seconds = '00';
-      const formatTime = `${year}-${month}-${day}   ${hours}:${minutes}:${seconds}`;
-      const questionId=data[i].question.id;
-      const answerId=data[i].id;
-      let copyRight= `\n\n知乎原文: [${title}](https://www.zhihu.com/question/${questionId}/answer/${answerId})`;
-      let header = `title: ${title}\n` + `date: ${formatTime} \n` + `categories: 知乎 \ndescription:  \n \n---\n\n\n `;
-      header = new Buffer(header);
-      copyRight=new Buffer(copyRight);
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-      }
+            time = parseInt(time);
+            const createdTime = new Date(time);
+            const year = createdTime.getFullYear();
+            let month = createdTime.getMonth() + 1;
+            month = (month < 10 ? `0${month}` : month);
+            let day = createdTime.getDate();
+            day = (day < 10 ? `0${day}` : day);
+            let hours = createdTime.getHours();
+            hours = (hours < 10 ? `0${hours}` : hours);
+            let minutes = createdTime.getMinutes();
+            minutes = (minutes < 10 ? `0${minutes}` : minutes);
+            const seconds = '00';
+            const formatTime = `${year}-${month}-${day}   ${hours}:${minutes}:${seconds}`;
+            const questionId = data[i].question.id;
+            const answerId = data[i].id;
+            let copyRight = `\n\n知乎原文: [${title}](https://www.zhihu.com/question/${questionId}/answer/${answerId})`;
+            let header = `title: ${title}\n` + `date: ${formatTime} \n` + 'categories: 知乎 \ndescription:  \n \n---\n\n\n ';
+            header = new Buffer(header);
+            copyRight = new Buffer(copyRight);
+            if (!fs.existsSync(dir)) {
+                fs.mkdirSync(dir);
+            }
             // 如果没有指定目录，创建之
-      fs.writeFile(`${dir}/${title}.md`, header, 'utf8', (err) => {
-        if (err) throw err;
-        //console.log('', i, '.md');
-      });
-      fs.appendFile(`${dir}/${title}.md`, answer+copyRight, 'utf8', (err) => {
-        if (err) throw err;
-        //console.log('write JSON into ', i, '.md');
-      });
-      
+            fs.writeFile(`${dir}/${title}.md`, header, 'utf8', (err) => {
+                if (err) throw err;
+                // console.log('', i, '.md');
+            });
+            fs.appendFile(`${dir}/${title}.md`, answer + copyRight, 'utf8', (err) => {
+                if (err) throw err;
+                // console.log('write JSON into ', i, '.md');
+            });
+        });
     });
-  });
 }
 console.log('done!');
